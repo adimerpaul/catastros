e65c7498c5c663f63ec0bfbe223099549dfa6fe7
adimerpaul <adimer101@gmail.com>
2021-03-01 15:42:59 -0400

    comenzado historial

--- resources/js/components/Historial.vue
+++ resources/js/components/Historial.vue
@@ -1,11 +1,26 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header bg-success text-white"> Sistemas seguimiento de archivos</div>
-                    <div class="card-body text-center" >
-                        <img src="img/gamo.png" class="img-fluid" alt="Responsive image">
+                    <div class="card-header bg-dark text-white"> <i class="fa fa-history"></i> Historial de un documento</div>
+                    <div class="card-body " >
+                        <div class="row">
+                            <div class="col-6">
+                                <form>
+                                    <div class="form-group row">
+                                        <label for="Numero" class="col-sm-3 col-form-label">Numero</label>
+                                        <div class="col-sm-5">
+                                            <input type="password" class="form-control" id="Numero" placeholder="Numero de documento">
+                                        </div>
+                                        <div class="col-sm-4">
+                                            <button ></button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
